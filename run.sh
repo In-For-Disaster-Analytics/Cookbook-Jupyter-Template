e5428da1c266fcb402834eef5b78b96dfbf01678
Maximiliano Osorio <maxiosorio@gmail.com>
2024-07-19 09:02:51 -0400

    fix: safe point

--- run.sh
+++ run.sh
@@ -1,95 +1,11 @@
 #!/bin/bash
 set -xe
 
-# Repository Variables
-# GIT_REPO_URL: URL of the repository to clone where the notebook and environment files are located
-# COOKBOOK_NAME: Name of the cookbook
-# COOKBOOK_CONDA_ENV: Name of the conda environment
-# IS_GPU_JOB: Boolean value to indicate if the job is a GPU job. If true, it will load the CUDA module
-export GIT_REPO_URL="https://github.com/In-For-Disaster-Analytics/Cookbook-Jupyter-Template.git"
-export COOKBOOK_NAME="cookbook-template-jupyter"
-export COOKBOOK_CONDA_ENV="example"
-IS_GPU_JOB=false
-
-
-# Cookbook Variables
-# DOWNLOAD_LATEST_VERSION: Boolean value to download the latest version of the repository
-# UPDATE_CONDA_ENV: Boolean value to update the conda environment
-# GIT_BRANCH: Branch of the repository to clone
-if [ "$1" != "true" ] && [ "$1" != "false" ]; then
-	echo "The first parameter must be a boolean value to recreate the environment"
-	exit 1
-fi
-if [ "$#" -ne 3 ]; then
-	echo "Illegal number of parameters"
-	exit 1
-fi
-
-export DOWNLOAD_LATEST_VERSION=$1
-export UPDATE_CONDA_ENV=$2
-export GIT_BRANCH=$3
-
-function install_conda() {
-	echo "Checking if miniconda3 is installed..."
-	if [ ! -d "$WORK/miniconda3" ]; then
-		echo "Miniconda not found in $WORK..."
-		echo "Installing..."
-		mkdir -p "$WORK/miniconda3"
-		curl https://repo.anaconda.com/miniconda/Miniconda3-py311_23.10.0-1-Linux-x86_64.sh -o "$WORK/miniconda3/miniconda.sh"
-		bash "$WORK/miniconda3/miniconda.sh" -b -u -p "$WORK/miniconda3"
-		rm -rf "$WORK/miniconda3/miniconda.sh"
-		export PATH="$WORK/miniconda3/bin:$PATH"
-		echo "Ensuring conda base environment is OFF..."
-		conda config --set auto_activate_base false
-	else
-		export PATH="$WORK/miniconda3/bin:$PATH"
-	fi
-	conda init bash
-	echo "Sourcing .bashrc..."
-	source ~/.bashrc
-	unset PYTHONPATH
-}
-
 function load_cuda() {
 	echo "Loading CUDA..."
 	module load cuda/12.0
 }
 
-function export_repo_variables() {
-	COOKBOOK_DIR=${WORK}/cookbooks
-	COOKBOOK_WORKSPACE_DIR=${COOKBOOK_DIR}/${COOKBOOK_NAME}
-	COOKBOOK_REPOSITORY_PARENT_DIR=${COOKBOOK_DIR}/.repository
-	COOKBOOK_REPOSITORY_DIR=${COOKBOOK_REPOSITORY_PARENT_DIR}/${COOKBOOK_NAME}
-	UPDATE_AVAILABLE_FILE=${COOKBOOK_WORKSPACE_DIR}/UPDATE_AVAILABLE.txt
-	NODE_HOSTNAME_PREFIX=$(hostname -s) # Short Host Name  -->  name of compute node: c###-###
-	NODE_HOSTNAME_DOMAIN=$(hostname -d) # DNS Name  -->  stampede2.tacc.utexas.edu
-	NODE_HOSTNAME_LONG=$(hostname -f)   # Fully Qualified Domain Name  -->  c###-###.stampede2.tacc.utexas.edu
-	export COOKBOOK_DIR
-	export COOKBOOK_WORKSPACE_DIR
-	export COOKBOOK_REPOSITORY_DIR
-	export COOKBOOK_REPOSITORY_PARENT_DIR
-	export UPDATE_AVAILABLE_FILE
-	export NODE_HOSTNAME_PREFIX
-	export NODE_HOSTNAME_DOMAIN
-	export NODE_HOSTNAME_LONG
-}
-
-function clone_cookbook_on_workspace() {
-	DATE_FILE_SUFFIX=$(date +%Y%m%d%H%M%S)
-	if [ ! -d "$COOKBOOK_WORKSPACE_DIR" ]; then
-		git clone ${GIT_REPO_URL} --branch ${GIT_BRANCH} ${COOKBOOK_WORKSPACE_DIR}
-	else
-		if [ ${DOWNLOAD_LATEST_VERSION} = "true" ]; then
-			mv ${COOKBOOK_WORKSPACE_DIR} ${COOKBOOK_WORKSPACE_DIR}-${DATE_FILE_SUFFIX}
-			git clone ${GIT_REPO_URL} --branch ${GIT_BRANCH} ${COOKBOOK_WORKSPACE_DIR}
-		fi
-	fi
-}
-
-function init_directory() {
-	mkdir -p ${COOKBOOK_REPOSITORY_PARENT_DIR}
-	clone_cookbook_on_workspace
-}
 
 function get_tap_certificate() {
 	mkdir -p ${HOME}/.tap # this should exist at this point, but just in case...
@@ -232,53 +148,7 @@ function session_cleanup() {
 	done
 }
 
-function conda_environment_exists() {
-	conda env list | grep "${COOKBOOK_CONDA_ENV}"
-}
-
-function create_conda_environment() {
-	if [ -f $COOKBOOK_WORKSPACE_DIR/.binder/environment.yml ]; then
-		conda env create -n ${COOKBOOK_CONDA_ENV} -f $COOKBOOK_WORKSPACE_DIR/.binder/environment.yml --force
-		conda activate ${COOKBOOK_CONDA_ENV}
-	elif  [ -f $COOKBOOK_WORKSPACE_DIR/.binder/environment.yaml ]; then
-		conda env create -n ${COOKBOOK_CONDA_ENV} -f $COOKBOOK_WORKSPACE_DIR/.binder/environment.yaml --force
-		conda activate ${COOKBOOK_CONDA_ENV}
-	fi
-	if [ -f $COOKBOOK_WORKSPACE_DIR/.binder/requirements.txt ]; then
-		pip install --no-cache-dir -r $COOKBOOK_WORKSPACE_DIR/.binder/requirements.txt
-	fi
-	python -m ipykernel install --user --name "${COOKBOOK_CONDA_ENV}" --display-name "Python (${COOKBOOK_CONDA_ENV})"
-}
-
-function delete_conda_environment() {
-	conda deactivate
-	conda env remove -n ${COOKBOOK_CONDA_ENV}
-}
-
-function handle_installation() {
-	if [ ${UPDATE_CONDA_ENV} = "true" ]; then
-		if { conda_environment_exists; } >/dev/null 2>&1; then
-			delete_conda_environment
-		fi
-		create_conda_environment
-	else
-		if { conda_environment_exists; } >/dev/null 2>&1; then
-			echo "Conda environment already exists"
-		else
-			create_conda_environment
-		fi
-	fi
-}
-
-
-
-#Execution
-install_conda
-if [ "$IS_GPU_JOB" = "true" ]; then
-	load_cuda
-fi
-export_repo_variables
-init_directory
+load_cuda
 load_tap_functions
 get_tap_certificate
 get_tap_token
